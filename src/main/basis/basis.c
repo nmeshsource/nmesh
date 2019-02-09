e5e9144edf9567b68d7e981536d430609be23be4
Wolfgang <wolf@fau.edu>
2019-02-09 13:46:32 -0500

    more

--- src/main/basis/basis.c
+++ src/main/basis/basis.c
@@ -11,13 +11,13 @@
 /***********************************************************************/
 
 /* get deriv in direction dir of array var, result goes into dvar */
-void basis_array_deriv(tNode *node, int dir, tArray *var, tArray *dvar)
+void basis_array_deriv1(tNode *node, int dir, tArray *var, tArray *dvar)
 {
   tArray *Dt = node->Dt[dir];
   mm_array_indir(Dt, var, dir, dvar);
 }
 /* get deriv in direction dir of var with index vi, result goes to var dvi */
-int basis_var_deriv(tNode *node, int dir, int vi, int dvi)
+int basis_var_deriv1(tNode *node, int dir, int vi, int dvi)
 {
   tArray *var, *dvar;
   tDat *dat = node->dat;
@@ -25,7 +25,27 @@ int basis_var_deriv(tNode *node, int dir, int vi, int dvi)
   {
     var  = dat->v[vi];
     dvar = dat->v[dvi];
-    basis_array_deriv(node, dir, var, dvar);
+    basis_array_deriv1(node, dir, var, dvar);
+    return 1;
+  }
+  else
+    return 0;
+}
+
+/* compute 1st derivs in all 3 dirs, result goes into vars dvi, dvi+1, dvi+2 */
+int basis_var_derivs(tNode *node, int vi, int dvi)
+{
+  tArray *var, *dvar;
+  tDat *dat = node->dat;
+  if(dat)
+  {
+    var  = dat->v[vi];
+    int dir;
+    for(dir=0; dir<3; dir++)
+    {
+      dvar = dat->v[dvi + dir];
+      basis_array_deriv1(node, dir, var, dvar);
+    }
     return 1;
   }
   else
@@ -37,13 +57,13 @@ int basis_var_deriv(tNode *node, int dir, int vi, int dvi)
 /***********************************************************************/
 
 /* get coeffs in direction dir of array var, coeffs goes into c */
-void basis_array_analysis(tNode *node, int dir, tArray *var, tArray *c)
+void basis_array_analysis1(tNode *node, int dir, tArray *var, tArray *c)
 {
   tArray *At = node->At[dir];
   mm_array_indir(At, var, dir, c);
 }
 /* get coeffs in direction dir of variable vi, coeffs goes into ci */
-int basis_var_analysis(tNode *node, int dir, int vi, int ci)
+int basis_var_analysis1(tNode *node, int dir, int vi, int ci)
 {
   tArray *var, *c;
   tDat *dat = node->dat;
@@ -51,7 +71,7 @@ int basis_var_analysis(tNode *node, int dir, int vi, int ci)
   {
     var = dat->v[vi];
     c   = dat->v[ci];
-    basis_array_analysis(node, dir, var, c);
+    basis_array_analysis1(node, dir, var, c);
     return 1;
   }
   else
@@ -59,13 +79,13 @@ int basis_var_analysis(tNode *node, int dir, int vi, int ci)
 }
 
 /* get array from coeffs c in direction dir, array goes into var */
-void basis_array_synthesis(tNode *node, int dir, tArray *var, tArray *c)
+void basis_array_synthesis1(tNode *node, int dir, tArray *var, tArray *c)
 {
   tArray *St = node->St[dir];
   mm_array_indir(St, c, dir, var);
 }
 /* get var vi from coeffs in var ci in direction dir */
-int basis_var_synthesis(tNode *node, int dir, int vi, int ci)
+int basis_var_synthesis1(tNode *node, int dir, int vi, int ci)
 {
   tArray *var, *c;
   tDat *dat = node->dat;
@@ -73,7 +93,7 @@ int basis_var_synthesis(tNode *node, int dir, int vi, int ci)
   {
     var = dat->v[vi];
     c   = dat->v[ci];
-    basis_array_synthesis(node, dir, var, c);
+    basis_array_synthesis1(node, dir, var, c);
     return 1;
   }
   else
