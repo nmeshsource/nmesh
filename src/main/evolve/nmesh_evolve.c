977ab1b819335b01c131dce6e736acd7e71e993b
Wolfgang Tichy <wolf@fau.edu>
2019-02-27 17:53:01 -0500

    more

---
+++ src/main/evolve/nmesh_evolve.c
@@ -0,0 +1,25 @@
+/* nmesh_evolve.h */
+/* (c) Wolfgang Tichy 2/2019 */
+
+#include "nmesh.h"
+#include "evolve.h"
+
+
+int nmesh_evolve(tMesh *mesh)
+{
+  printf("Adding evolve\n");
+
+  /* functions */
+  AddFun(INITIALDATA, evolve_test);
+
+  /* variables */
+  AddEvoVar("evolve_u", "", "some var");
+  AddEvoVar("evolve_v", "", "another var");
+  //AddVarDim("evolve_us", "", "that needs surface exchange",7,0,6);
+
+  /* parameters */
+  //AddPar("evolve_lowlatency", "no", 
+  //       "send many small rather than few large messages");
+
+  return 0;
+}
