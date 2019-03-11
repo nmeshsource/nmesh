4003167a5a75d30764d263189acdf8820218a9ff
wolf <wolf@fau.edu>
2019-03-11 13:03:26 -0400

    fix warning

--- src/main/coordinates/nmesh_coordinates.h
+++ src/main/coordinates/nmesh_coordinates.h
@@ -70,8 +70,8 @@ int cart_partials(tNode *node, int ui, int dui);
 /* utilities.c */
 double patch_normal_at_XYZ(tPat *pat, int f, const double X[3], double n[3]);
 double node_normal_at_ijk(tNode *node, int f, int ijk, double n[3]);
-double det_3Dmatrix(const double M[3][3]);
-double inv3Dmat_from_3Dmat(const double M[3][3], double invM[3][3]);
+double det_3Dmatrix(double M[3][3]);
+double inv3Dmat_from_3Dmat(double M[3][3], double invM[3][3]);
 double smallest_pat_size(tMesh *mesh);
 
 /* ComplexFunctions.c */
