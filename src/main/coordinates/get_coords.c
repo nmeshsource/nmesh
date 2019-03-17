86a3525fed63e97257adb3c00106242cadff1672
Wolfgang <wolf@fau.edu>
2019-03-17 11:18:23 -0400

    remove some old code

--- src/main/coordinates/get_coords.c
+++ src/main/coordinates/get_coords.c
@@ -375,73 +375,6 @@ int fnb_containing_point(tNode *node, int f,
 }
 
 
-/* Mark all points in aC[0..1] that are in node->fnb[f][ni] by writing
-   their index into aI. If a point is not in the node we write -1
-   into aI. I.e. we return a mask of points in node in Ip.
-   Note that aC and aoC contain the same points but in different coords.
-   aC in X coords of the node and aoC in X coords of the neighbor. */
-void brct_mark_points_in_fnb_f_ni(tNode *node, int f, int ni, tArray *aC[2],
-                                  tArray *aoC[2], tArray *aI)
-{
-  tPat *pat = node->pat;
-  tNode *nb = node->fnb[f][ni];
-  tBface *obface;
-  tPat *opat;
-  int odir, od1, od2;
-  double nbrct[4];
-  int k;
-
-  forarray(aC[0], k)
-  {
-    double C[]  = {  aC[0]->d[k],  aC[1]->d[k] };
-    double oC[] = { aoC[0]->d[k], aoC[1]->d[k] };
-
-    /* find bface on other side with C */
-    obface = nbbface_of_bface_containing_point(nb, pat, f, C);
-    if(!obface)
-    {
-      aI->i[k] = -1;
-      continue;
-    }
-
-    /* get patch on other side */
-    opat = obface->pat;
-    if(!opat)
-    {
-      aI->i[k] = -1;
-      continue;
-    }
-
-    /* get bounding rectangle of the node nb */
-    odir = obface->f/2;
-    od1 = Dir1_norm(odir);
-    od2 = Dir2_norm(odir);
-    nbrct[0] = nb->bbox[2*od1];
-    nbrct[1] = nb->bbox[2*od1+1];
-    nbrct[2] = nb->bbox[2*od2];
-    nbrct[3] = nb->bbox[2*od2+1];
-
-    if(0) //(f==1 && node->nid==1)
-    {
-      char s[100], snb[100];
-      printf("ni=%d: %s  %s ", ni, nodename(node,s,99), nodename(nb,snb,99));
-      prbbox(nbrct,2);printf("\n");
-      printf("nb->pat=%d opat=%d  oC=%g %g  in=%d\n",
-      nb->pat->p, opat->p, oC[0],oC[1], C_in_brct(nbrct , oC));
-      //printnode(node);
-      //printbfaces_on_f(node->pat, f);
-      //printnode(nb);
-      //printbfaces_on_f(nb->pat, nb_f);
-    }
-
-    /* check if C from opat is within bounding rectangle of nb */
-    if( (nb->pat == opat) && (C_in_brct(nbrct , oC)) )
-      aI->i[k] = k;
-    else
-      aI->i[k] = -1;
-  }
-}
-
 /* Mark all points in aC[0..1] of node that are in node nb by writing
    their index into aI. If a point is not in the node we write -1
    into aI. I.e. we return a mask of points in node in Ip.
@@ -455,6 +388,10 @@ void mark_points_in_nb_f(tNode *node, int f, tArray *aC[2],
   int odir, od1, od2;
   double nbrct[4];
   int k;
+
+  errorexit("mark_points_in_nb_f checks only 2 out of 3 coords!!! "
+            "This is not good enough in many cases-> FIXME!!! "
+            "Probably bound rect. should not be used for this...");
 
   /* get bounding rectangle of the node nb */
   odir = nb_f/2;
@@ -980,78 +917,6 @@ int C_in_brct(const double brct[4], double C[2])
     if(C[d] > brct[2*d+1]) C[d] = brct[2*d+1];
   }
   return 1;
-}
-
-
-/* convert X-coords on face f of node to X-coords of neighboring node,
-   and write them into nbC */
-void array_nbXface_of_Xface(tNode *node, int f,
-                            tNode *nb, int nb_f, tArray *nbC[2])
-{
-  tPat *pat = node->pat;
-  tMesh *mesh = pat->mesh;
-  int *n = node->n;
-  int dir = f/2;
-  int pl = (n[dir]-1)*(f%2);
-  //int d1 = Dir1_norm(dir);
-  //int d2 = Dir2_norm(dir);
-  int iX = Ind("X");
-  int ix = Ind("x");
-  double *pX[] = { Vard(node,iX), Vard(node,iX+1), Vard(node,iX+2) };
-  double *px[] = { Vard(node,ix), Vard(node,ix+1), Vard(node,ix+2) };
-  double *oC[] = { Arrd(nbC[0]), Arrd(nbC[1]) };
-  int i,j,k, p;
-  tPat *opat = nb->pat;
-  int odir = nb_f/2;
-  int od1 = Dir1_norm(odir);
-  int od2 = Dir2_norm(odir);
-  double nbrct[4];
-
-  forplaneN(dir, i,j,k, n, pl)
-  {
-    int ijk = Ind_n(i,j,k, n);
-    int ind = Ind_n_norm(i,j,k, n, dir);
-    double x[] = { px[0][ijk], px[1][ijk], px[2][ijk] };
-    double X[] = { pX[0][ijk], pX[1][ijk], pX[2][ijk] };
-    //double C[] = { X[d1], X[d2] };
-    double oX[3];
-
-    p = p_XYZ_of_xyz(opat, oX, x);
-    if(p<0)
-    {
-      /* we couldn't find, the nb point oX, but maybe the two coords on the
-         face are ok? */
-      double oCl[] = { oX[od1], oX[od2] };
-
-      brct_nodeface(nb, odir, nbrct);
-      if(!C_in_brct(nbrct, oCl))
-      {
-        /* just use something outside the nb's bound. rect., to signal to
-           e.g. mark_points_in_nb_f that this is not be used */
-        oX[0] = nb->bbox[1] * 2.;
-        oX[1] = nb->bbox[3] * 2.;
-        oX[2] = nb->bbox[5] * 2.;
-      }
-    }
-    oC[0][ind] = oX[od1];
-    oC[1][ind] = oX[od2];
-
-    if(0) // if(p<0)
-    {
-      printnode(node);
-      printf("f=%d  ", f);
-      pr3v("X", X);
-      pr3v("x", x);
-      printf("\n");
-      printnode(nb);
-      //printbface(obface);
-      //printf("obface->f=%d  ", obface->f);
-      pr3v("oX", oX);
-      printf("\nopat:  ");
-      printpatch(opat);
-      errorexit("x should be in opat!!!");
-    }
-  }
 }
 
 
--- src/main/coordinates/nmesh_coordinates.h
+++ src/main/coordinates/nmesh_coordinates.h
@@ -64,8 +64,6 @@ void mark_points_in_fnb_f_ni(tNode *node, int f, int ni, tArray *aC[2],
                              tArray *aoC[2], tArray *aI);
 void mark_points_in_nb_f(tNode *node, int f, tArray *aC[2],
                          tNode *nb, int nb_f, tArray *aoC[2], tArray *aI);
-void array_nbXface_of_Xface(tNode *node, int f,
-                            tNode *nb, int nb_f, tArray *nbC[2]);
 void array_find_nbXface_of_Xface(tNode *node, int f, tNode *nb, int nb_f,
                                  tArray *nbC[2], tArray *nbI);
 
