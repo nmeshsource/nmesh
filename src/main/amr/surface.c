c7804109582396a9c57dc6c1724e227b1ec03033
Wolfgang <wolf@fau.edu>
2019-03-17 21:20:07 -0400

    switch off copy for now since it does not seem faster

--- src/main/amr/surface.c
+++ src/main/amr/surface.c
@@ -638,7 +638,7 @@ void set_ajsurf_forall_vars(tNode *node, int f)
       if(!found) errorexit("couldn't find nb face!!!");
 
       /* if nb has only node as neighbor we may not need to interpolate */
-      if(nb->nfnb[nb_f] == 1)
+      if(0 && nb->nfnb[nb_f] == 1)
       {
         tMesh *mesh = node->pat->mesh;
         int ix = Ind("x");
