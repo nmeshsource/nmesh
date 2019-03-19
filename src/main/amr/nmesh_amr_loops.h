318937aabfb7ab96cfbda42b2aa1f2f4bc265fea
Wolfgang Tichy <wolf@fau.edu>
2019-03-19 16:58:49 -0400

    fix loop for n=1 case

--- src/main/amr/nmesh_amr_loops.h
+++ src/main/amr/nmesh_amr_loops.h
@@ -164,9 +164,9 @@
 
 /* like forplaneN but loop only over 4 corners */
 #define forcornersN(N, i,j,k, n, p) \
-  for(k=(p)*((N)==2); ( k<(n[2]) ) && ( ( k==(p) ) || (N)!=2 ); k+=(n[2])-1) \
-  for(j=(p)*((N)==1); ( j<(n[1]) ) && ( ( j==(p) ) || (N)!=1 ); j+=(n[1])-1) \
-  for(i=(p)*((N)==0); ( i<(n[0]) ) && ( ( i==(p) ) || (N)!=0 ); i+=(n[0])-1)
+  for(k=(p)*((N)==2); (k<n[2]) && (k==(p) || (N)!=2); k+=(n[2])-(n[2]>1)) \
+  for(j=(p)*((N)==1); (j<n[1]) && (j==(p) || (N)!=1); j+=(n[1])-(n[1]>1)) \
+  for(i=(p)*((N)==0); (i<n[0]) && (i==(p) || (N)!=0); i+=(n[0])-(n[0]>1))
 
 /* same as forplaneN, but omit edges */
 #define forinnerplaneN(N, i,j,k, n, p) \
