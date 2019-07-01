6676ba680e1bbd17d192a8387fac0ac4c9740c61
Wolfgang Tichy <wolf@fau.edu>
2019-07-01 12:01:11 -0400

    more

--- src/main/evolve/nmesh_evolve.h
+++ src/main/evolve/nmesh_evolve.h
@@ -5,6 +5,9 @@
 
 
 /* evolve.c */
+void evolve_register_subsys(tMesh *mesh, tVarList *u,
+                FuncPointer prelim, FuncPointer limdata, FuncPointer limiter,
+                FuncPointer presurf, FuncPointer setsrc, FuncPointer setrhs);
 void evolve_register_subsys_u_rhs_src_lim(tMesh *mesh, tVarList *u,
                                           FuncPointer rhs, FuncPointer src,
                                           FuncPointer limdata,
