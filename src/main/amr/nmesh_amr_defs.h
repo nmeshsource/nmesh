ea0a6782c9eef2e675f9b5c6f7a7093816e901e0
Wolfgang Tichy <wolf@fau.edu>
2019-07-08 16:41:40 -0400

    add defs for stack arrays (maybe faster)

---
+++ src/main/amr/nmesh_amr_defs.h
@@ -0,0 +1,28 @@
+/* nmesh_amr_defs.h */
+/* Wolfgang Tichy, 7/2019 */
+
+
+/* Arrays on the stack instead of the heap.
+   instead of:
+    tArray *array = alloc_array_with_segs(n, Ne, ns);
+   we can use:
+    DECL_STACK_ARRAY_with_segs(array, n, Ne, ns);
+ */
+#define DECL_STACK_ARRAY_with_segs(array, n_, Ne_, ns_) \
+  tArray array[1]; \
+  array->N = n_[0] * n_[1] * n_[2]; \
+  for(int i=0; i<3; i++)  array->n[i] = n_[i]; \
+  array->Ne = (Ne_); \
+  array->ns = (ns_); \
+  array->d_nofree = 1; \
+  array->si = array->info = 0; \
+  double array##data_[(array->N + (Ne_)) * (ns_)]; \
+  array->size = sizeof(array##data_); \
+  array->d = &(array##data_[0])
+
+/* instead of:
+    tArray *array = alloc_array_with_segs(n);
+   we can use:
+    DECL_STACK_ARRAY(array, n);
+ */
+#define DECL_STACK_ARRAY(array, n_) STACK_ARRAY_with_segs(array, n_, 0, 1)
