73f8aa4c22acfcf11336c18fd295e8c19e9e04c3
Wolfgang Tichy <wolf@fau.edu>
2019-07-12 15:17:47 -0400

    1 more test

--- src/utility/numerics/test_rootfinder.c
+++ src/utility/numerics/test_rootfinder.c
@@ -55,6 +55,11 @@ int main()
   printf("find_2roots_region ret=%d: x0[0]=%.16g x0[1]=%.16g\n\n",
          ret, x0[0], x0[1]);
 
+  x1=3.01; x2=7; x0[0]=6.5;
+  ret = find_2roots_region(x0, f, x1,x2, 0, maxits,tol,pr);
+  printf("find_2roots_region ret=%d: x0[0]=%.16g x0[1]=%.16g\n\n",
+         ret, x0[0], x0[1]);
+
   x1=1.5; x2=4; x0[0]=3.9;
   ret = rtbrent_brak(x0, f, x1,x2, 0, maxits,tol,pr);
   printf("rtbrent_brak ret=%d: x0[0]=%.16g\n\n", ret, x0[0]);
