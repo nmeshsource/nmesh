1dfc15d443e97633e15dc0bc27803fdef7449c44
Wolfgang Tichy <wolf@fau.edu>
2019-08-18 19:48:53 -0400

    fix printf

--- src/main/checkpoint/checkpoint_load.c
+++ src/main/checkpoint/checkpoint_load.c
@@ -208,12 +208,10 @@ int checkpoint_load_nodes(tMesh *mesh, char *fname)
         }
       }
       fclose(fp);
+      PRF;printf(": mesh->iteration=%d mesh->time=%g\n",
+                 mesh->iteration, mesh->time);
+      fflush(stdout);
     }
-
-    PRF;printf(": mesh->iteration=%d mesh->time=%g\n",
-               mesh->iteration, mesh->time);
-    fflush(stdout);
-
     /* wait until everyone is here */
     nMPI_barrier();
   } /* end rk-loop */
