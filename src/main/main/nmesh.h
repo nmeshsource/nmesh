0a3a9f45079480496017ce9c1dedb28487654a5d
Wolfgang Tichy <wolf@fau.edu>
2019-10-16 17:08:10 -0400

    increase _XOPEN_SOURCE from 500 to 600
    The value 600 (corresponding to the sixth revision) includes definitions
    from SUSv3.

--- src/main/main/nmesh.h
+++ src/main/main/nmesh.h
@@ -2,7 +2,7 @@
 /* Wolfgang Tichy, 1/2019 */
 
 /* _XOPEN_SOURCE>=500 needed for strdup */
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 600
 /* to use _XOPEN_SOURCE we need to include nmesh.h before all other headers
    in files such as main/main/utilities.c or main/main/parameters.c */
 
