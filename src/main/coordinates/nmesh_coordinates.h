0518d2163a9a0202651c0745f34e957471d86e38
Wolfgang Tichy <wolf@fau.edu>
2020-06-05 00:19:02 -0400

    add global func declaration

--- src/main/coordinates/nmesh_coordinates.h
+++ src/main/coordinates/nmesh_coordinates.h
@@ -60,6 +60,7 @@ int p_XYZ_of_xyz(tPat *pat, double X[3], const double x[3]);
 int p_XYZ_of_xyz_inpatlist(tMesh *mesh, intList *pl,
                            double X[3], const double x[3]);
 int p_XYZ_of_xyz_mesh(tMesh *mesh, double X[3], const double x[3]);
+tNode *node_XYZ_of_xyz_mesh(tMesh *mesh, double X[3], const double x[3]);
 long l_XYZ_of_xyz(tNode *node, int ind, double X[3], const double x[3]);
 int XYZ_on_face(tPat *pat, int *face, const double X[3]);
 int ind_on_nodeface(tNode *node, int ind, int *face);
