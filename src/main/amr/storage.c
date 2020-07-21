0f337e3c8950a353ce05ec0f151c56be487141bc
Wolfgang Tichy <wolf@fau.edu>
2020-07-21 16:40:12 -0400

    omp critical (make_or_destroy_nodes) doesn't work with -fsanitize=thread

--- src/main/amr/storage.c
+++ src/main/amr/storage.c
@@ -471,7 +471,11 @@ tNlist *make8_child_nodes(tNode *parent, int n[3])
   the descends local. So that locking nearest neighbors would be effective.
   */
 
-  //#pragma omp critical (make_or_destroy_nodes)
+  /* #pragma omp critical (make_or_destroy_nodes) */
+  /* NOTE: For some reason gcc's -fsanitize=thread throws a false positive
+           if I use a named critical section.
+           So replace "GEN_Pragma(omp critical (make_or_destroy_nodes))"
+           by "GEN_Pragma(omp critical)" when debugging races!!! */
   GEN_Pragma(omp critical (make_or_destroy_nodes))
   {
     ///* aquire lock for change of connections */
@@ -719,7 +723,11 @@ tNode *destroy_children(tNode *parent)
   locked with parent_and_fnbs_lock.
   */
 
-  //#pragma omp critical (make_or_destroy_nodes)
+  /* #pragma omp critical (make_or_destroy_nodes) */
+  /* NOTE: For some reason gcc's -fsanitize=thread throws a false positive
+           if I use a named critical section.
+           So replace "GEN_Pragma(omp critical (make_or_destroy_nodes))"
+           by "GEN_Pragma(omp critical)" when debugging races!!! */
   GEN_Pragma(omp critical (make_or_destroy_nodes))
   {
     ///* obtain lock on face neighbors of narray in name of parent */
