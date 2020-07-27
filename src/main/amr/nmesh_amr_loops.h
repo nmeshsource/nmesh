019ec962dde967034c12253ed8c7f76c710876bc
Wolfgang Tichy <wolf@fau.edu>
2020-07-28 01:03:38 +0200

    change formylnodes if USE_OMP_TASKLOOP is defined
    Now we use the equivalent to "#pragma omp parallel master taskloop".

--- src/main/amr/nmesh_amr_loops.h
+++ src/main/amr/nmesh_amr_loops.h
@@ -28,23 +28,25 @@
 
 /* we use OpenMP to parallelize the 2nd loop in formylnodes_noomp */
 #define formylnodes(mesh) \
-  NODELEVEL_Pragma(omp parallel) \
-  NODELEVEL_Pragma(omp single) \
   for(int li_, cat_=0; cat_ < mesh->myln->nncats; cat_++) \
+  NODELEVEL_Pragma(omp parallel) \
+  NODELEVEL_Pragma(omp master) \
   NODELEVEL_Pragma(omp taskloop) \
   for(li_=0; li_ < mesh->myln->ncat[cat_]; li_++)
 
 /* this one will have no "parallel" on its own */
 /*
 #define formylnodes_ompfor(mesh) \
-  NODELEVEL_Pragma(omp single) \
   for(int li_, cat_=0; cat_ < mesh->myln->nncats; cat_++) \
+  NODELEVEL_Pragma(omp master) \
   NODELEVEL_Pragma(omp taskloop) \
   for(li_=0; li_ < mesh->myln->ncat[cat_]; li_++)
 */
-/* THIS DOESN'T WORK in dg_add_surface_fluxes!!! It causes a race for e.g.
+/* THIS DIDN'T WORK in dg_add_surface_fluxes!!! It caused a race for e.g.
    dgi->node = node; ... It seems tasks get processed by arbitrary threads so
-   that allocating dgi on a per thread basis is not good enough... */
+   that allocating dgi on a per thread basis is not good enough...
+   BUT try it again now that "omp master","omp taskloop" are inside the
+   cat_-loop!!! */
 /* to start tasks formylnodes_ompfor has to be inside a:
    #pragma omp parallel {  } */
 
