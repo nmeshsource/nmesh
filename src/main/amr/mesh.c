bdd22e6c1f7a7ee0363febbc58f24f472d262e17
Wolfgang <wolf@fau.edu>
2020-11-10 11:42:11 -0500

    fix typo

--- src/main/amr/mesh.c
+++ src/main/amr/mesh.c
@@ -322,7 +322,7 @@ int setup_CubedSphere_mesh(tMesh *mesh)
   double csize = 0.375; //extent of inner cubes from center (must be below ~1/sqrt(3))
   double ssfac = Getd(Par("amr_CubedSphere_r0fac")); //DNSdata_OuterShellStart
   double obfac = Getd(Par("amr_CubedSphere_r1fac")); //DNSdata_OuterBoundary
-  int stretch  = Getd(Par("amr_OuterShell_stretch"));
+  int stretch  = Getb(Par("amr_OuterShell_stretch"));
   double rc[3];
   double ABrct[] = { -1.,1., -1.,1. };
   double xc[] = { 0., 0., 0. };
