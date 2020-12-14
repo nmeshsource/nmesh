ae47ac883e7fad33ba0aa77b4bf3e7673962c394
Wolfgang <wolf@fau.edu>
2020-12-13 23:13:14 -0500

    more

--- src/main/dg/nmesh_dg.h
+++ src/main/dg/nmesh_dg.h
@@ -72,3 +72,5 @@ double rec1d_p_WENO3_2g(int n, const double *u, int im, double u_scale);
 double rec1d_m_WENO3_2g(int n, const double *u, int im, double u_scale);
 void rec1d_uface_to_uin_1(tNode *node, tVarList *vlu, int forward);
 void rec1d_uface_to_uin_1_mesh(tMesh *mesh, tVarList *vlu, int forward);
+double rec1d_p_WENOm3_2(int n, const double *u, int im, double u_scale);
+double rec1d_m_WENOm3_2(int n, const double *u, int im, double u_scale);
