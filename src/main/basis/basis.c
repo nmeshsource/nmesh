nmeshsource/nmesh

commit 15b63968218533531bf853b748e50e9406389510

catch a few corner cases

src/utility/output/output0d.c
@@ -78,8 +78,8 @@ void output0d_mesh_vl(tVarList *vl, tPat *pat, int It, double T)
     int vi = vl->index[vli];
     char *name = VarName(vi);
 
-    /* get volume only once */
-    if(vli==0) Vol = MeshVolumeIntegral(mesh,pat, vi, 0.,0);
+    /* get volume only once, and use var X to the power 0.0 to compute it */
+    if(vli==0) Vol = MeshVolumeIntegral(mesh,pat, Ind("X"),0.0 ,0);
 
     VolInt = MeshVolumeIntegral(mesh,pat, vi, 1.,0);
     mean   = VolInt/Vol;
@@ -105,7 +105,12 @@ void output0d_mesh_vl(tVarList *vl, tPat *pat, int It, double T)
     for(ipt=0; ipt<Npt; ipt++)
     {
       if(node_pt[ipt])
-        val_pt[ipt] = basis_var_interpolate(node_pt[ipt], vi, Xb_pt[ipt]);
+      {
+        if(VarA(node_pt[ipt], vi))
+          val_pt[ipt] = basis_var_interpolate(node_pt[ipt], vi, Xb_pt[ipt]);
+        else
+          val_pt[ipt] = 0.; /* set to zero if var vi has no storage */
+      }
     }
 
 

src/main/main/norms.c
@@ -18,8 +18,8 @@ double MeshVolumeIntegral(tMesh *mesh, tPat *pat, int vind,
 
     if(pat && node->pat != pat) continue;
 
-    VolInt += NodeVolumeIntegral(node, vind, power, mode);
-
+    if(VarA(node, vind)) /* only count nodes where var vind has storage */
+      VolInt += NodeVolumeIntegral(node, vind, power, mode);
   }
   sum = VolInt;
   nMPI_Allreduce(&VolInt, &sum, 1, nMPI_DOUBLE, nMPI_SUM);
@@ -44,6 +44,8 @@ double MeshMaxLoc_local(tMesh *mesh, tPat *pat, int vind,
     int ijk;
 
     if(pat && node->pat != pat) continue;
+
+    if(VarA(node,vind) == NULL) continue;
 
     nmax = max_array(VarA(node,vind), &ijk);
     if(nmax > max)
@@ -73,6 +75,8 @@ double MeshMinLoc_local(tMesh *mesh, tPat *pat, int vind,
     int ijk;
 
     if(pat && node->pat != pat) continue;
+
+    if(VarA(node,vind) == NULL) continue;
 
     nmin = min_array(VarA(node,vind), &ijk);
     if(nmin < min)

commit 8ea018afe5b33571d8f9c5502f47b90df97ad74d

fix MPI bug I introduced in 15b63968218

src/main/basis/basis.c
@@ -221,6 +221,7 @@ double basis_var_interpolate_local(tNode *node, int vi, double Xb[3])
 
   /* set coeffs of var vi in c */
   v = VarA(node, vi);
+  if(!v) return 0.; /* return 0 as interp value if var vi has no storage */
   c = alloc_array(node->n);
   basis_array_analysis3(node, v, c);
 

src/utility/output/output0d.c
@@ -105,12 +105,7 @@ void output0d_mesh_vl(tVarList *vl, tPat *pat, int It, double T)
     for(ipt=0; ipt<Npt; ipt++)
     {
       if(node_pt[ipt])
-      {
-        if(VarA(node_pt[ipt], vi))
-          val_pt[ipt] = basis_var_interpolate(node_pt[ipt], vi, Xb_pt[ipt]);
-        else
-          val_pt[ipt] = 0.; /* set to zero if var vi has no storage */
-      }
+        val_pt[ipt] = basis_var_interpolate(node_pt[ipt], vi, Xb_pt[ipt]);
     }
 
 