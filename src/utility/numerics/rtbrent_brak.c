4dee2b8209691d77abcef125d9a4a0a2e9170c09
Wolfgang Tichy <wolf@fau.edu>
2021-01-05 16:01:19 -0500

    more info in rtbrent_brak

--- src/utility/numerics/rtbrent_brak.c
+++ src/utility/numerics/rtbrent_brak.c
@@ -36,8 +36,11 @@ int rtbrent_brak(double *x0, double (*func)(double,void *par),
   /* check if *x0 is within [x1,x2] */
   if((fa < 0. && fb < 0.) || (fa > 0. && fb > 0.))
   {
-    if(pr) printf("rtbrent_brak: Root is not bracketed!  "
-                  "fa=%g fb=%g\n", fa,fb);
+    if(pr)
+    {
+      printf("rtbrent_brak: Root is not bracketed!  fa=%g fb=%g\n", fa,fb);
+      printf("  a=x1=%g b=x2=%g\n", a,b);
+    }
     return -2*maxits-3;
   }
 
