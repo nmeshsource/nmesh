a5912f1f3039970203edec274e549f5b72c28bda
Wolfgang Tichy <wolf@fau.edu>
2021-09-09 00:55:22 -0400

    more

--- src/main/main/banparameters.c
+++ src/main/main/banparameters.c
@@ -5,7 +5,7 @@
 #include "nmesh.h"
 
 
-/* structure for one blacklisted parameter */
+/* structure for one banned parameter */
 typedef struct tBANNEDPAR {
   const char *name;
   const char *ban_reason;
@@ -20,7 +20,7 @@ typedef tBannedPar *pBanned;  /* list_templates.h only works with numbers */
 #undef TYP
 
 
-/* global list with blacklisted pars */
+/* global list with banned pars */
 pBannedList *bannedpars = NULL;
 
 
@@ -29,14 +29,23 @@ pBannedList *bannedpars = NULL;
    because they have been removed, or are considered bad somehow */
 /**************************************************************************/
 
+void prFreeBannedParList(void)
+{
+  int k;
+  if(bannedpars)
+    forList(bannedpars, k)
+      printf("%s %s\n", ListEntry(bannedpars,k)->name,
+                        ListEntry(bannedpars,k)->ban_reason);
+}
+
 /* compare name in name_ob with name in bp,
    return 1 if names are equal, return 0 otherwise */
 int prop_samenames(const void *name_ob, tBannedPar *bp)
 {
   const char *name = (const char *) name_ob;
+  printf("strcmp(%s, %s);\n", bp->name, name);
   return ( strcmp(name, bp->name)==0 );
 }
-
 
 /* put an outdated par in a blacklist */
 void BanPar(const char *name, const char *ban_reason)
@@ -48,11 +57,10 @@ void BanPar(const char *name, const char *ban_reason)
 
   if(0) {PRF;printf("%s %s\n", name, ban_reason);}
 
-
   /* look for name in bannedpars */
   i = index_prop_pBannedList(bannedpars,0, prop_samenames, name);
   if(i>=0)
-    errorexit("cannot blacklist the same par twice");
+    errorexit("cannot ban the same par twice");
 
   /* add par name to bannedpars */
   bp = calloc(1, 1);
@@ -65,15 +73,17 @@ void BanPar(const char *name, const char *ban_reason)
 /* look for par name in bannedpars */
 int ExitIfParBanned(const char *name)
 {
+  //prFreeBannedParList();
   if(bannedpars)
   {
     /* look for name in bannedpars */
     int i = index_prop_pBannedList(bannedpars,0, prop_samenames, name);
+    PRF;printf(": %s -> i=%d\n", name, i);
     if(i>=0)
     {
-      printf("Parameter %s is blacklisted:\n", name);
+      printf("Parameter %s is banned:\n", name);
       printf("%s\n", bannedpars->e[i]->ban_reason);
-      errorexits("Parameter %s is blacklisted", name);
+      errorexits("Parameter %s is banned", name);
     }
     /* i=-1 means par name was not found */
   }
