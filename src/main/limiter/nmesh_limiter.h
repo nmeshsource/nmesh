e6ae2211b288e72c815a21d0949d91be75295abe
Wolfgang Tichy <wolf@fau.edu>
2021-09-24 13:41:34 -0400

    put MRS_phiy, MRS_theta_Mml, minmod3, minmod3B in global header file

--- src/main/limiter/nmesh_limiter.h
+++ src/main/limiter/nmesh_limiter.h
@@ -2,7 +2,12 @@
 /* Wolfgang Tichy, April 2005 */
 
 
+/* limiter.c */
 int limdata_MRS(tNode *node, tVarList *vl);
 int limiter_MRS(tNode *node, tVarList *vl);
 int limdata_c000_100_010_001(tNode *node, tVarList *vl);
 int limiter_minmodB(tNode *node, tVarList *vl);
+double MRS_phiy(double y);
+double MRS_theta_Mml(double Mi, double wbar, double wMi);
+double minmod3(double a, double b, double c);
+double minmod3B(double a, double b, double c,  double aBound);
