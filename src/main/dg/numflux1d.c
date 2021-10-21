07dfe67717f0b0e667c887e181eac5d520d29b94
Wolfgang Tichy <wolf@fau.edu>
2021-10-20 21:34:23 -0400

    minor

--- src/main/dg/numflux1d.c
+++ src/main/dg/numflux1d.c
@@ -87,7 +87,7 @@ void numflux1d_LLF(tDGinfo *d)
 
   /* build fluxes */
   for(i=0; i<nf; i++) 
-    d->fnum[i] = 0.5*( d->fa[i] +  d->fi[i] - amax*(d->ua[i] - d->ui[i]) );
+    d->fnum[i] = 0.5*( d->fa[i] + d->fi[i] - amax*(d->ua[i] - d->ui[i]) );
 
 //PRF;printf(": d->fnum =");
 //for(i=0; i<nf; i++) printf(" %g", d->fnum[i]);
