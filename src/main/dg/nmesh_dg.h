e5c4fc08b44f2610895035d352f1c6d579b7101b
Wolfgang Tichy <wolf@fau.edu>
2022-04-02 19:58:00 -0400

    add dissipation_add_taperedKO_order

--- src/main/dg/nmesh_dg.h
+++ src/main/dg/nmesh_dg.h
@@ -149,3 +149,5 @@ void dissipation_add_KO4_mesh(tMesh *mesh, tVarList *vlr, tVarList *vlu,
                               double dissfac);
 void dissipation_add_KO_order(tNode *node, tVarList *vlr, tVarList *vlu,
                               double dissfac, int order);
+void dissipation_add_taperedKO_order(tNode *node, tVarList *vlr, tVarList *vlu,
+                                     double dissfac, int order, double *dfac);
