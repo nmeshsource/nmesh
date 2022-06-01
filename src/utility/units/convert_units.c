6adbbe1d777bc3ae1d6ffc054929f6b1d8b5ae7d
Wolfgang Tichy <wolf@fau.edu>
2022-05-31 23:19:12 -0400

    minor

--- src/utility/units/convert_units.c
+++ src/utility/units/convert_units.c
@@ -18,9 +18,9 @@ struct tNATURECONSTS
 /* values of consts of nature we use */
 struct tNATURECONSTS natureconsts =
 {
-  .c     = 299792458,        // [m/s],          defined
+  .c     = 299792458,        // [m/s],     c is defined to have this value
   .G     = 6.67430e-11,      // [m^3/(kg s^2)], from https://physics.nist.gov/cgi-bin/cuu/Value?bg
-  .GMsun = 1.32712440018e20, // [m^3/s^2],      mesured using Kepler's law
+  .GMsun = 1.32712440018e20, // [m^3/s^2], standard gravitational parameter, measured using Kepler's law
 };
 
 
