7b9d02ffd92e11e23200f53974b6bf744084de09
Wolfgang Tichy <wolf@fau.edu>
2022-07-04 17:35:53 -0400

    minor

--- src/tests/scalarwave1/scalarwave1.c
+++ src/tests/scalarwave1/scalarwave1.c
@@ -146,7 +146,7 @@ void scalarwave1_fluxes_pt(tDGinfo *d)
   int dir = f/2;
   int ijk = Ind_n(d->i,d->j,d->k, n);
   int JK = Ind_n_norm(d->i,d->j,d->k, n, dir);
-  int calc_aj = !( (d->info) & DGINFO_INNONLY );
+  int set_aj = !( (d->info) & DGINFO_INNONLY );
   int nvars = vlu->n;
   double norm[3];
   int l;
@@ -158,7 +158,7 @@ void scalarwave1_fluxes_pt(tDGinfo *d)
 
   /* eigenval in dir norm */
   scalarwave1_eigenval1d(node,nvars, d->lami,norm);
-  if(calc_aj) scalarwave1_eigenval1d(node,nvars, d->lama,norm);
+  if(set_aj) scalarwave1_eigenval1d(node,nvars, d->lama,norm);
 
   /* reset d->Ffac to default */
   d->Ffac = 1.;
@@ -169,7 +169,7 @@ void scalarwave1_fluxes_pt(tDGinfo *d)
     int vi = Vind(vlu,l);
 
     u = Vard_(node, vi);
-    if(calc_aj) uaj = Varaj(node, vi, f);
+    if(set_aj) uaj = Varaj(node, vi, f);
 
     /* cons var inside node */
     d->ui[l] = u[ijk];
@@ -180,7 +180,7 @@ void scalarwave1_fluxes_pt(tDGinfo *d)
   }
 
   /* no adjacent u, i.e. we are on outer boundary */
-  if(!uaj && calc_aj) /* we check the last uaj here. */
+  if(!uaj && set_aj) /* we check the last uaj here. */
   {
     int ix = Ind("x");
     double *x = Vard_(node, ix);
@@ -207,7 +207,7 @@ void scalarwave1_fluxes_pt(tDGinfo *d)
 
   /* get inner and adjacent fluxes fi, fa */
   scalarwave1_flux1d(node,nvars, d->fi,norm, d->ui);
-  if(calc_aj) scalarwave1_flux1d(node,nvars, d->fa,norm, d->ua);
+  if(set_aj) scalarwave1_flux1d(node,nvars, d->fa,norm, d->ua);
 }
 
 
