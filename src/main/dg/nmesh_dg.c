e5f56cce6b4c4d94501914733d79cecfd148e844
Wolfgang Tichy <wolf@fau.edu>
2022-07-05 13:17:34 -0400

    let fv_test_divf be evo var to have surfaces

--- src/main/dg/nmesh_dg.c
+++ src/main/dg/nmesh_dg.c
@@ -52,7 +52,7 @@ int nmesh_dg(tMesh *mesh)
   if(Getb(Par("fv_tests")))
   {
     AddFun(EVOLVE, fv_tests); //put in EVOLVE since fv_divf needs nb. surf
-    AddAuxVar("fv_test_divf", "i", "d_k f^k(u_i), where we simply set "
+    AddEvoVar("fv_test_divf", "i", "d_k f^k(u_i), where we simply set "
               "f^k(u_x) = (1,0,0), f^k(u_y) = (0,1,0), f^k(u_z) = (0,0,1) so "
               "d_k f^k(u_i) = (1/J) d_{kb} (J sqrt(g^{kb,kb} n^{kb}_i) "
               "n^{kb}_i = i-component of normal in dir Xb^{k}");
