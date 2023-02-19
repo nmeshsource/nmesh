74aad2309ba9e20a3574abbe4b33dd64cf0e8ce2
Wolfgang <wolf@fau.edu>
2023-02-18 23:27:40 -0500

    wip

--- src/utility/output/gnuplot.c
+++ src/utility/output/gnuplot.c
@@ -267,7 +267,6 @@ void gnuplot_output1d_perpat_meshvar(tMesh *mesh, char *name,
                                      int It, double T)
 {
   int vi = Ind(name);
-  FILE *fX, *fY, *fZ;
   char Xfil[1000];
   char Yfil[1000];
   char Zfil[1000];
@@ -284,6 +283,9 @@ void gnuplot_output1d_perpat_meshvar(tMesh *mesh, char *name,
     /* do work when it is my turn */
     if(rk == nMPI_rank())
     {
+      FILE *fX=NULL, *fY=NULL, *fZ=NULL;
+      int poldx=-1, poldy=-1, poldz=-1;
+
       /* loop over all leaf nodes */
       formylnodes_noomp(mesh)
       {
@@ -294,8 +296,9 @@ void gnuplot_output1d_perpat_meshvar(tMesh *mesh, char *name,
         {
           int p = node->pat->p;
           int ijk[3];
+          int writTx, writTy, writTz;
 
-          //TODO: use diffferent Xb0 for diff patches
+          //TODO: use different Xb0 for diff patches
           double X0[] = { Getd(Par("outputX0")),
                           Getd(Par("outputY0")),
                           Getd(Par("outputZ0")) };
@@ -308,37 +311,58 @@ void gnuplot_output1d_perpat_meshvar(tMesh *mesh, char *name,
           /* X-axis:  Y = Y0, Z = Z0 */
           if(ijk[1]>=0 && ijk[2]>=0)
           {
-            snprintf(Xfil, 999, fmt, Gets(Par("outdir")),name, p, "X");
-            fX = fopen(Xfil, "a");
-            if(!fX) errorexits("failed opening %s", Xfil);
-            if(IObufsz) setvbuf(fX, IObuf, _IOFBF, IObufsz);
-            write_line_ascii(node, fX, 0, ijk, VarA(node, vi), It,T,1);
-            fclose(fX);
+            if(p != poldx)
+            {
+              poldx = p;
+              if(fX) fclose(fX);
+              snprintf(Xfil, 999, fmt, Gets(Par("outdir")),name, p, "X");
+              fX = fopen(Xfil, "a");
+              if(!fX) errorexits("failed opening %s", Xfil);
+              if(IObufsz) setvbuf(fX, IObuf, _IOFBF, IObufsz);
+              writTx=1; /* add line with time*/
+            }
+            write_line_ascii(node, fX, 0, ijk, VarA(node, vi), It,T,writTx);
+            writTx=0;
           }
 
           /* Y-axis:  X = X0, Z = Z0 */
           if(ijk[0]>=0 && ijk[2]>=0)
           {
-            snprintf(Yfil, 999, fmt, Gets(Par("outdir")),name, p, "Y");
-            fY = fopen(Yfil, "a");
-            if(!fY) errorexits("failed opening %s", Yfil);
-            if(IObufsz) setvbuf(fY, IObuf, _IOFBF, IObufsz);
-            write_line_ascii(node, fY, 1, ijk, VarA(node, vi), It,T,1);
-            fclose(fY);
+            if(p != poldy)
+            {
+              poldy = p;
+              if(fY) fclose(fY);
+              snprintf(Yfil, 999, fmt, Gets(Par("outdir")),name, p, "Y");
+              fY = fopen(Yfil, "a");
+              if(!fY) errorexits("failed opening %s", Yfil);
+              if(IObufsz) setvbuf(fY, IObuf, _IOFBF, IObufsz);
+              writTy=1; /* add line with time*/
+            }
+            write_line_ascii(node, fY, 1, ijk, VarA(node, vi), It,T,writTy);
+            writTy=0;
           }
 
           /* Z-axis:  X = X0, Y = Y0 */
           if(ijk[0]>=0 && ijk[1]>=0)
           {
-            snprintf(Zfil, 999, fmt, Gets(Par("outdir")),name, p, "Z");
-            fZ = fopen(Zfil, "a");
-            if(!fZ) errorexits("failed opening %s", Zfil);
-            if(IObufsz) setvbuf(fZ, IObuf, _IOFBF, IObufsz);
-            write_line_ascii(node, fZ, 2, ijk, VarA(node, vi), It,T,1);
-            fclose(fZ);
+            if(p != poldz)
+            {
+              poldz = p;
+              if(fZ) fclose(fZ);
+              snprintf(Zfil, 999, fmt, Gets(Par("outdir")),name, p, "Z");
+              fZ = fopen(Zfil, "a");
+              if(!fZ) errorexits("failed opening %s", Zfil);
+              if(IObufsz) setvbuf(fZ, IObuf, _IOFBF, IObufsz);
+              writTz=1; /* add line with time*/
+            }
+            write_line_ascii(node, fZ, 2, ijk, VarA(node, vi), It,T,writTz);
+            writTz=0;
           }
         }
       } /* end formylnodes_noomp */
+      if(fX) fclose(fX);
+      if(fY) fclose(fY);
+      if(fZ) fclose(fZ);
       fs_sync(mesh); /* make sure every MPI proc flushes buffers to disk */
     }
     /* wait until everyone is here */
