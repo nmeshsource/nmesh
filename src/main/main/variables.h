1dff55d12ed035edef3dabd572c54c0b5bbc0dae
Wolfgang Tichy <wolf@fau.edu>
2023-05-03 18:20:59 -0400

    better names in src/main/main/variables.h

--- src/main/main/variables.h
+++ src/main/main/variables.h
@@ -2,10 +2,12 @@
 /* Wolfgang Tichy, 5/2019 */
 
 /* for variable types used in variables.c */
+
 enum
 {
-  EVOVAR=0,  //legacy warning: we may use 0 instead of EVOVAR somwhere
-  AUXVAR=1,  //legacy warning: we may use 1 instead of AUXVAR somwhere
-  DATAVAR=2,
-  EXCHVAR=4
+  EVOVAR=0,  //loadbal transfer via MPI, interp on refine, exchange surfaces
+  AUXVAR=1,  //no loadbal transfer, no interp on refine, no surfaces
+  DATAVAR=2, //loadbal transfer via MPI and interp on refine, but no surfaces
+  LBTVAR=4   //loadbal transfer via MPI, but no interp. and no surfaces
 };
+//Note: we may use 0,1 instead of EVOVAR,EVOVAR somwhere
