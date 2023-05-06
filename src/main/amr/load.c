9ad5078265c2c10f1009cb6dd3a738b855faf42a
Wolfgang <wolf@fau.edu>
2023-05-06 16:18:34 -0400

    fix stupid compiler warning in load.c

--- src/main/amr/load.c
+++ src/main/amr/load.c
@@ -891,8 +891,9 @@ ops_bal_sum, ops_elm_sum, max, diff, ret);
   return ret;
 }
 
-/* find rank we want to put elm on */
-int load_desired_rank(int size, const double *ops_bal_sum, double ops_elm_sum)
+/* find rank we want to put elm on
+   In: size,ops_bal_sum  =>  Out: ops_elm_sum */
+int load_desired_rank(int size, double *ops_bal_sum, double ops_elm_sum)
 {
   void *val;
   double max = ops_bal_sum[size-1];
