36ded831cf37b05cbda6a4fb8078953d8af095ec
Wolfgang Tichy <wolf@fau.edu>
2023-05-16 00:15:29 -0400

    minor

--- src/main/amr/connect.c
+++ src/main/amr/connect.c
@@ -899,7 +899,7 @@ void remove_unpaired_node_fnbs_only(tNode *node)
         remove_node_fnb_only(node, f, ni);
         ni--;
 
-if(node->nid==72)
+if(Node_eid(nb)==72)
 {
 Yo(3.0);
 printnode(node);
