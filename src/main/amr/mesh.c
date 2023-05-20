11ed09647122e36b3ece24bd56d96c37935e8827
Wolfgang Tichy <wolf@fau.edu>
2023-05-20 12:23:15 -0400

    more

--- src/main/amr/mesh.c
+++ src/main/amr/mesh.c
@@ -813,6 +813,26 @@ Yo(33.2);
   }
 
 
+  amr_remove_mesh_nbelm(mesh);
+  Timing->child1to7_weight = 0.;
+  load_balance_elms(mesh);
+  alloc_and_set_mesh_myelm(mesh);
+
+  amr_elm_nbinfo_to_elm_fnb(mesh);
+  amr_elm_nbinfo_set_nnbinfo_mesh(mesh, 1); //make nnbinfo positive
+
+  amr_get_nbelm_elmheaders(mesh);
+
+  printf("mesh->myelm:\n");
+  printelmarray(mesh->nmyelm, mesh->myelm);
+  printmyelms(mesh);
+
+  printf("mesh->nbelm:\n");
+  printelmarray(mesh->nnbelm, mesh->nbelm);
+  printnbelms(mesh);
+
+
+
 
 //nMPI
 nMPI_barrier();
