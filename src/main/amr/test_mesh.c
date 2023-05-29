00a33fad116d9d06863466a23205db2fe9f01914
Wolfgang Tichy <wolf@fau.edu>
2023-05-28 21:05:01 -0400

    wip

--- src/main/amr/test_mesh.c
+++ src/main/amr/test_mesh.c
@@ -12,7 +12,8 @@
 /* refine a randomly chosen fraction frac of nodes, do this N times */
 void random_refine_frac_nodes_N_times(tMesh *mesh, double frac, int N)
 {
-  uint32_t ran=1; // seed for rand_32primitive
+  uint32_t ran1=1; // seed1 for rand_32primitive
+  uint32_t ran2=1; // seed2 for rand_32primitive
   tRef ref[1];
   int i;
 
@@ -23,16 +24,18 @@ void random_refine_frac_nodes_N_times(tMesh *mesh, double frac, int N)
     formylnodes(mesh)
     {
       tNode *node = MyLnode;
-      node->rflag = rand_32primitive_u01(&ran) + frac;
+      node->rflag = rand_32primitive_u01(&ran2) + frac;
     }
-    ref->type = rand_32primitive_u01(&ran) * 1.99999;
+    ref->type = rand_32primitive_u01(&ran1) * 1.99999;
     ref->method = GIVEN_n;
-    ref->n[0] = rand_32primitive_u01(&ran) * 6 + 1;
-    ref->n[1] = rand_32primitive_u01(&ran) * 6 + 1;
-    ref->n[2] = rand_32primitive_u01(&ran) * 6 + 1;
+    ref->n[0] = rand_32primitive_u01(&ran1) * 6 + 1;
+    ref->n[1] = rand_32primitive_u01(&ran1) * 6 + 1;
+    ref->n[2] = rand_32primitive_u01(&ran1) * 6 + 1;
     //hp_refine_nodes_if_rflag(mesh, ref);
     if(ref->type==0) hrefine_nodes_if_rflag(mesh, ref);
     else             prefine_nodes_if_rflag(mesh, ref);
+    update_mesh_myln_node_nid(mesh);
+    simple_load_balance(mesh);
     update_mesh_myln_node_nid(mesh);
   }
 }
@@ -40,7 +43,8 @@ void random_refine_frac_nodes_N_times(tMesh *mesh, double frac, int N)
 /* unrefine a randomly chosen fraction frac of nodes, do this N times */
 void random_remove_frac_nodes_N_times(tMesh *mesh, double frac, int N)
 {
-  uint32_t ran=1; // seed for rand_32primitive
+  uint32_t ran1=1; // seed1 for rand_32primitive
+  uint32_t ran2=1; // seed2 for rand_32primitive
   tRef ref[1];
   int i, j, rflag;
 
@@ -52,16 +56,18 @@ void random_remove_frac_nodes_N_times(tMesh *mesh, double frac, int N)
     formylnodes(mesh)
     {
       tNode *node = MyLnode;
-      if(j%8==0) rflag = -(rand_32primitive_u01(&ran) + frac);
+      if(j%8==0) rflag = -(rand_32primitive_u01(&ran2) + frac);
       node->rflag = rflag * (Node_l(node)>0);
       j++;
     }
-    //ref->type = rand_32primitive_u01(&ran) * 1.99999;
+    //ref->type = rand_32primitive_u01(&ran1) * 1.99999;
     ref->method = GIVEN_n;
-    ref->n[0] = rand_32primitive_u01(&ran) * 6 + 1;
-    ref->n[1] = rand_32primitive_u01(&ran) * 6 + 1;
-    ref->n[2] = rand_32primitive_u01(&ran) * 6 + 1;
+    ref->n[0] = rand_32primitive_u01(&ran1) * 6 + 1;
+    ref->n[1] = rand_32primitive_u01(&ran1) * 6 + 1;
+    ref->n[2] = rand_32primitive_u01(&ran1) * 6 + 1;
     remove_nodes_if_rflag(mesh, ref);
+    update_mesh_myln_node_nid(mesh);
+    simple_load_balance(mesh);
     update_mesh_myln_node_nid(mesh);
   }
 }
@@ -72,6 +78,8 @@ void random_remove_frac_nodes_N_times(tMesh *mesh, double frac, int N)
 int test_mesh(tMesh *mesh)
 {
   int mode=1;
+  tRef ref[1];
+
   PRFs(":\n");
 
   mesh->dt = Getd(Par("dt"));
@@ -87,6 +95,16 @@ int test_mesh(tMesh *mesh)
   /* setup all bfaces and root node connections */
   amr_set_bfaces_and_rnode_nfaces_fnb(mesh, 1);
   update_mesh_myln_node_nid(mesh);
+
+  /* refine all once to have more nodes */
+  formylnodes(mesh)
+  {
+    tNode *node = MyLnode;
+    node->rflag = 1;
+  }
+  ref->method = PARENT_n;
+  hrefine_nodes_if_rflag(mesh, ref);
+  update_mesh_myln_node_nid(mesh);
   simple_load_balance(mesh);
   update_mesh_myln_node_nid(mesh);
 
@@ -94,16 +112,14 @@ int test_mesh(tMesh *mesh)
   write_mylnodes(mesh, "initial patches:", mode);
 
   /* 1. random refine */
-  random_refine_frac_nodes_N_times(mesh, 0.2, 12);
-  simple_load_balance(mesh);
-  update_mesh_myln_node_nid(mesh);
+  random_refine_frac_nodes_N_times(mesh, 0.2, 8);
   write_mylnodes(mesh, "ref1:", mode);
 
+  //does not work:
   /* 2. random unrefine */
-  random_remove_frac_nodes_N_times(mesh, 0.2, 12);
-  simple_load_balance(mesh);
-  update_mesh_myln_node_nid(mesh);
-  write_mylnodes(mesh, "ref2:", mode);
+  //random_remove_frac_nodes_N_times(mesh, 0.2, 9);
+  //write_mylnodes(mesh, "ref2:", mode);
+  //write_mylnodes crashes while printing a fnb is messed up (e.g. pat=NULL)
 
   return 0;
 }
