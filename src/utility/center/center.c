d6b389d2b6161675dccb5958c02c696c1e18d9e2
Wolfgang Tichy <wolf@fau.edu>
2024-01-21 10:18:36 -0500

    fix typo

--- src/utility/center/center.c
+++ src/utility/center/center.c
@@ -23,11 +23,11 @@ int center_init_globals(tMesh *mesh)
   center->center2_z = Par("center2_z");
 
   /* make sure some pars are saved in checkpoint */
-  if(Geti(Par("center0_track_method"))
+  if(Geti(Par("center0_track_method")))
     checkpoint_save_pars_append(mesh, "center0_x center0_y center0_z");
-  if(Geti(Par("center1_track_method"))
+  if(Geti(Par("center1_track_method")))
     checkpoint_save_pars_append(mesh, "center1_x center1_y center1_z");
-  if(Geti(Par("center2_track_method"))
+  if(Geti(Par("center2_track_method")))
     checkpoint_save_pars_append(mesh, "center2_x center2_y center2_z");
 
   return 0;
