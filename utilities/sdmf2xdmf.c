7f3ee6279ad7a176d7b826c2bbcc360f992f2a0a
Wolfgang Tichy <wolf@fau.edu>
2024-02-07 09:52:52 -0500

    add utility to convert sdmf .txt files to xdmf .xmf files

---
+++ utilities/sdmf2xdmf.c
@@ -0,0 +1,143 @@
+/* Parse output of diff and check for differences in floating point numbers */
+/* (c) Wolfgang Tichy 2002 */
+
+#define STRLEN 65536
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../src/utility/output/XDMF_out.h"
+
+
+int main(int argc, char *argv[])
+{
+  char *textname;
+  char xmfname[STRLEN];
+  FILE *ftxt;
+  FILE *fxmf;
+  int slen;
+  char line[STRLEN];
+  char *pstr;
+  char str[STRLEN];
+  char str1[STRLEN];
+  char str2[STRLEN];
+  char vname[STRLEN];
+  char suffix[STRLEN];
+  char fname[STRLEN];
+  char fname_xyz[STRLEN];
+  char TopologyType[STRLEN];
+  char AttributeCenter[STRLEN];
+  int first_time;
+
+  /* read args */
+  if(argc!=2)
+  {
+    printf("Usage: sdmf2xdmf file.txt\n");
+    return -1;
+  }
+  textname = argv[1];
+
+  /* open textname */
+  ftxt = fopen(textname, "r");
+  if(!ftxt)
+  {
+    printf("%s not found. \n", textname);
+    return -1;
+  }
+  /* read header in textname */
+  fgets(line,STRLEN, ftxt);
+  pstr = strstr(line, "TopologyType:");
+  sscanf(pstr,"%s %s", str1, TopologyType);
+  pstr = strstr(line, "AttributeCenter:");
+  sscanf(pstr,"%s %s", str1, AttributeCenter);
+  fgets(line,STRLEN, ftxt);
+
+  /* construct xmfname */
+  strcpy(xmfname, textname);
+  slen = strlen(xmfname);
+  xmfname[slen-3] = 0;
+  strcat(xmfname, "xmf");
+
+  /* open xmfname */
+  fxmf = fopen(xmfname, "w");
+  if(!ftxt)
+  {
+    printf("%s not found. \n", xmfname);
+    return -1;
+  }
+  /* write header */
+  fprintf(fxmf, "%s", B_head_xmf);
+  fprintf(fxmf, "%s", B_temporal_xmf);
+
+  /* get vname, suffix */
+  strcpy(vname, textname);
+  pstr = strstr(vname, ".");
+  pstr[0] = 0;
+  pstr++;
+  strcpy(suffix, pstr);
+  pstr = strstr(suffix, ".");
+  pstr[0] = 0;
+
+  /* filenames for field and also xyz data */
+  snprintf(fname, 1000, "%s.%s.bin", vname, suffix);
+  snprintf(fname_xyz, 1000, "xyz.%s.bin", suffix);
+
+  /* now go over textname */
+  first_time = 1;
+  while(fgets(line,STRLEN, ftxt))
+  {
+    char nodename[STRLEN];
+    double time;
+    int np;
+    int n[3];
+    int bin = 1;
+    const char *format = (bin) ? "Binary" : "XML";
+    long xyzoffset, voffset;
+
+    if(line[0]=='\n')
+    {
+      if(first_time)
+        first_time = 0;
+      else /* close previous time */
+        fprintf(fxmf, E_spatial_xmf);
+
+      /* new time series starts, so get its time */
+      fgets(line,STRLEN, ftxt);
+      sscanf(line,"%s %s %s %lf", str, str1, str2, &time);
+      fprintf(fxmf, B_spatial_xmf, time);
+      fgets(line,STRLEN, ftxt);
+    }
+
+    /* read line with node info */
+    sscanf(line,"%s  %d %d %d  %ld %ld",
+           nodename, &n[0],&n[1],&n[2], &xyzoffset, &voffset);
+    np = n[0]*n[1]*n[2];
+
+    /* print node info, as in write_xdmf_xmf of src/utility/output/XDMF_out.c */
+    fprintf(fxmf, B_E_grid_xmf,
+            nodename,
+            time,
+            np, 3,
+            format,
+            xyzoffset,
+            fname_xyz,
+            n[2], n[1], n[0],
+            vname,
+            n[2], n[1], n[0],
+            format,
+            voffset,
+            fname);
+  }
+
+  /* close previous time */
+  fprintf(fxmf, E_spatial_xmf);
+  fprintf(fxmf, E_temporal_xmf);
+
+  /* write end marker */
+  fprintf(fxmf, "%s", E_head_xmf);
+
+  fclose(fxmf);
+  fclose(ftxt);
+
+  return 0;
+}
