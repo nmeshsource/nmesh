c892711d0a7f55506c6411ebe9548a53be4d8f35
Wolfgang Tichy <wolf@fau.edu>
2024-02-07 07:41:04 -0500

    wip

--- src/utility/output/XDMF_out.c
+++ src/utility/output/XDMF_out.c
@@ -13,65 +13,12 @@
    Some description about XDMF is on
    http://www.xdmf.org
    http://www.xdmf.org/index.php/XDMF_Model_and_Format
-   http://www.paraview.org/Wiki/ParaView/Data_formats
+   http://www.paraview.org/Wiki/ParaView/Data_formats */
 
-   What we have here is quite similar to what is in bamps 4.0. But I added
-   "Time Value" and "Spatial" to B_spatial. Now VisIt shows the correct time
-   and paraview can show more than t=0. */
+/* include _xmf and _smf format strings: */
+#include "XDMF_out.h"
 
-
-
-/* XML format strings to make .xmf files using fprintf,
-   based on bamps and https://www.paraview.org/Wiki/ParaView/Data_formats */
-static const char *B_head_xmf =
-  "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-  "<Xdmf xmlns:xi=\"http://www.w3.org/2001/XInclude\" Version=\"2.1\">\n"
-  "  <Domain>\n";
-static const char *E_head_xmf =
-  "  </Domain>\n"
-  "</Xdmf>\n";
-
-static const char *B_temporal_xmf =
-  "    <Grid CollectionType=\"Temporal\" GridType=\"Collection\" Name=\"TCollection\">\n"
-  "      <Geometry Type=\"None\"/>\n"
-  "      <Topology Dimensions=\"0\" Type=\"NoTopology\"/>\n";
-static const char *E_temporal_xmf =
-  "    </Grid>\n";
-
-static const char *B_spatial_xmf =
-  "      <Grid CollectionType=\"Spatial\" GridType=\"Collection\" Name=\"SCollection\">\n"
-  "        <Time Value=\"%.9f\"/>\n"
-  "        <Geometry Type=\"None\"/>\n"
-  "        <Topology Dimensions=\"0\" Type=\"NoTopology\"/>\n";
-static const char *E_spatial_xmf =
-  "      </Grid>\n";
-
-static const char *B_E_grid_xmf =
-  "        <Grid Name=\"%s\">\n"
-  "          <Time Value=\"%.9f\"/>\n"
-  "          <Geometry Type=\"XYZ\">\n"
-  "            <DataItem DataType=\"Float\" Dimensions=\"%d %d\" Format=\"%s\" Seek=\"%ld\" Precision=\"4\">\n"
-  "              %s\n"
-  "            </DataItem>\n"
-  "          </Geometry>\n"
-  "          <Topology Dimensions=\"%d %d %d\" Type=\"3DSMesh\"/>\n"
-  "          <Attribute Center=\"Node\" Name=\"%s\" Type=\"Scalar\">\n"
-  "            <DataItem DataType=\"Float\" Dimensions=\"%d %d %d\" Format=\"%s\" Seek=\"%ld\" Precision=\"4\">\n"
-  "              %s\n"
-  "            </DataItem>\n"
-  "          </Attribute>\n"
-  "        </Grid>\n";
-
-/* same format strings, but in WT's simplified text format */
-static const char *B_head_smf = "# node n[0] n[1] n[2] xyzseek varseek\n";
-static const char *E_head_smf = "";
-static const char *B_temporal_smf = "";
-static const char *E_temporal_smf = "";
-static const char *B_spatial_smf = "\n# \"time = %.16g\"\n";
-static const char *E_spatial_smf = "";
-static const char *B_E_grid_smf = "%s\t%d %d %d\t%ld %ld\n";
-
-/* pointers that can point to the above _xmf or _smf strings */
+/* pointers that can point to the _xmf or _smf strings in XDMF_out.h */
 static const char *B_head;
 static const char *E_head;
 static const char *B_temporal;
