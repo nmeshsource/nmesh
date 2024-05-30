a4453698dcd257c8174c351fc1dd1cc330e847b0
Wolfgang Tichy <wolf@fau.edu>
2024-05-30 19:44:29 -0400

    better print

--- src/main/amr/load.c
+++ src/main/amr/load.c
@@ -69,7 +69,7 @@ int load_balance_if_needed(tMesh *mesh)
     // ^FIXME: load_set_desrank_ns_elms in load_balance calls this again...
     timing_set_maxops(mesh);
 
-    printTiming();
+    PRFs(": ");printTiming();
     timing_print_load(mesh);
 
     if(timing_ops2load(Timing->maxops) > Getd(Par("amr_loadbalance_maxload")))
