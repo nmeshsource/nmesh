f3a08fb3e3dc98bac4d4ee76ab273f5520d131e0
Wolfgang <wolf@@fau.edu>
2024-06-15 11:20:24 -0400

    add pars for WENOm: fv_rec_WENOm_s1, fv_rec_WENOm_s2, fv_rec_WENOm_opt
    With these we can control if e.g. WENOm3 uses 1st or 2nd order rec
    near the boundary

--- src/main/dg/nmesh_dg.c
+++ src/main/dg/nmesh_dg.c
@@ -33,6 +33,12 @@ int nmesh_dg(tMesh *mesh)
          "WENOm3,WENOm5,WENOmZ,"
          "WENO3if1away_1,WENO3if2away_1,WENO3_2,WENO3_2g]");
   AddPar("fv_WENOm3_opt_weightratio", "2", "2=standard, 3=mod value");
+  AddPar("fv_rec_WENOm_s1", "0", "use 0th order extrapolation if "
+         "|u1-u0|/|u2-u1| < s1, [0, 0.5, ...]");
+  AddPar("fv_rec_WENOm_s2", "DBL_MAX", "use 0th order extrapolation if "
+         "|u1-u0|/|u2-u1| > s2, [1e50, 1.5, ...]");
+  AddPar("fv_rec_WENOm_opt", "1", "possible options to fall back to 0th "
+         "order extrapolation [0,-1,1,2] (see rec1d_u_in1_weightfac)");
   AddPar("fv_flux", "fnum", "[fnum_minus_fi, fnum]");
   AddPar("fv_divf_extrap", "dnfn_extrap1", "how we extrap div(f) [no,"
          "divf_extrap1,dnfn_extrap1]");
