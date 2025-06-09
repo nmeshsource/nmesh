614f28eff7ddd8a685ea0e63780412cdaaaa6a7f
Wolfgang Tichy <wolf@fau.edu>
2025-06-09 13:51:45 -0400

    add a checkpoint test to advection1/unittests

--- src/tests/advection1/unittests/resetunits.sh
+++ src/tests/advection1/unittests/resetunits.sh
@@ -1,8 +1,11 @@
 TOP=../../../..
 
-rm -rf *_correct *_previous
+rm -rf *_correct *_previous  Chkpt_AtEnd
 
 NMESH=$TOP/exe/nmesh
 $NMESH advDG-FD.par		> advDG-FD.out
+$NMESH Chkpt_AtEnd.par		> Chkpt_AtEnd.out
 
 mv -v advDG-FD			advDG-FD_correct
+mv -v Chkpt_AtEnd		Chkpt_AtEnd_correct
+# we intend to compare the result of Chkpt_AtEnd.par with Chkpt_EvryIt.par
--- src/tests/advection1/unittests/unittests.sh
+++ src/tests/advection1/unittests/unittests.sh
@@ -3,5 +3,17 @@ TOP=../../../..
 NMESH=$TOP/exe/nmesh
 FDIFF=$TOP/utilities/floatdiff
 
+rm -rf Chkpt_EvryIt
+
 $NMESH advDG-FD.par > advDG-FD.out
+$NMESH Chkpt_EvryIt.par > Chkpt_EvryIt.out-1
+$NMESH Chkpt_EvryIt.par > Chkpt_EvryIt.out0
+$NMESH Chkpt_EvryIt.par > Chkpt_EvryIt.out1
+$NMESH Chkpt_EvryIt.par > Chkpt_EvryIt.out2
+
+rm -f Chkpt_AtEnd_correct/Chkpt_AtEnd.par
+rm -f Chkpt_EvryIt/Chkpt_EvryIt.par
+
 $FDIFF -at 1e-13 -rt 1e-12 advDG-FD_correct advDG-FD
+$FDIFF -at 1e-13 -rt 1e-12 Chkpt_AtEnd_correct Chkpt_EvryIt
+# we compare the result of Chkpt_AtEnd.par with Chkpt_EvryIt.par
