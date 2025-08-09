f0e6b278918e1dd90429ddceae1d3ffb2ec0a291
Wolfgang <wolf@fau.edu>
2025-08-08 23:10:13 -0400

    replace I,J,K by II,JJ,KK in Ind_n_norm

--- src/main/amr/amr.c
+++ src/main/amr/amr.c
@@ -229,31 +229,31 @@ void MPIexchange_free_all_myln(tMesh *mesh)
 /* index, but reduced by dimension along norm */
 int Ind_n_norm(int i, int j, int k, int n[3], int norm)
 {
-  int N0,N1, I,J,K;
+  int N0,N1, II,JJ,KK;
 
   N0 = n[0];
   N1 = n[1];
-  I = i;
-  J = j;
-  K = k;
+  II = i;
+  JJ = j;
+  KK = k;
 
   switch(norm)
   {
   case 0:
     N0 = 1;
-    I = 0;
+    II = 0;
     break;
   case 1:
     N1 = 1;
-    J = 0;
+    JJ = 0;
     break;
   case 2:
-    K = 0;
+    KK = 0;
     break;
   default:
     errorexit("norm needs to be 0,1,2");
   }
-  return I + N0*(J + N1*K);
+  return II + N0*(JJ + N1*KK);
 }
 
 
