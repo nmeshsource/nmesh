4a6091cce4337256be1d594b89f51873cc12a40d
Wolfgang Tichy <wolf@fau.edu>
2026-05-19 12:05:11 -0400

    minor

--- src/main/basis/gridpoints.c
+++ src/main/basis/gridpoints.c
@@ -214,7 +214,8 @@ int gridpoints_init(tMesh *mesh)
       if(r_WT)
       {
         nWT = atoi(r_WT+4);
-        if(!(nWT%2)) errorexit("the number after r_WT must be odd");
+        if( nWT>3 && (!(nWT%2)) )
+          errorexit("the number after r_WT must be odd (or 2)");
       }
 
       /* set rq */
