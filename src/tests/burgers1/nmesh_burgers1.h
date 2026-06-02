d37759dee1ad322235999755005bfd9e622abe02
wolf <wolf@tyrfing>
2026-06-02 19:56:38 -0400

    make burgers1_profile2_u_from_xt available to all modules

--- src/tests/burgers1/nmesh_burgers1.h
+++ src/tests/burgers1/nmesh_burgers1.h
@@ -1,3 +1,7 @@
 /* nmesh_burgers1.h */
 /* (c) Wolfgang Tichy 2/2019 */
 /* header file for global functions */
+
+
+/* burgers1.c */
+int burgers1_profile2_u_from_xt(double x, double t, double *u);
