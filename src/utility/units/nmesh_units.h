21e0e75e18fc217f0391f8f9bdac3a8fac824caa
wolf <wolf@tyrfing>
2026-07-01 16:00:32 -0400

    more

--- src/utility/units/nmesh_units.h
+++ src/utility/units/nmesh_units.h
@@ -1,2 +1,6 @@
 /* nmesh_units.h */
 /* Wolfgang Tichy, June 2022 */
+
+/* convert_units.c */
+/* include convert_units.h to use everything in convert_units.c */
+#include "convert_units.h"
--- src/utility/units/units.h
+++ src/utility/units/units.h
@@ -1,10 +1,6 @@
 /* units.h */
 /* Wolfgang Tichy, June 2022 */
 
-/* convert_units.c */
-/* include convert_units.h to use everything in convert_units.c */
-#include "convert_units.h"
-
 /* units.c */
 int units_set_for_GMc1_mesh(tMesh *mesh);
 int print_unit_conversion_factors(tMesh *mesh);
