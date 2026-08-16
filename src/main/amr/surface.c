7f76c90a58868659df1af25fffb621e35b124303
Wolfgang <wolf@fau.edu>
2026-08-16 13:12:41 -0400

    minor

--- src/main/amr/surface.c
+++ src/main/amr/surface.c
@@ -59,7 +59,7 @@ tVarList *vlalloc_AddVarsWithSurfacezones(tMesh *mesh)
 
 /**********************************************************************/
 /* NOTE: The functions below loop only over a varlist (not all vars).
-   But even so, all vars that with MeshVarSurfacezones(mesh,vi)=0 etc. (see
+   But even so, all vars with MeshVarSurfacezones(mesh,vi)=0 etc. (see
    e.g. init_surface) are still not included. This mat not be what we
    intend! It maybe be better to add surfaces to all vars in the varlist
    even if they have MeshVarSurfacezones(mesh,vi)=0. */
