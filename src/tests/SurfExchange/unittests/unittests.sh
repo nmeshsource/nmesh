f90ae10433bab9a1f2119841190a2108b48a0cf9
wolf <wolf@tyrfing>
2026-08-17 13:43:25 -0400

    wip

--- src/tests/SurfExchange/unittests/unittests.sh
+++ src/tests/SurfExchange/unittests/unittests.sh
@@ -7,9 +7,9 @@ FDIFF=$TOP/utilities/floatdiff
 /bin/cp SurfExchange_unit1.par SurfExchange_unit1_p2.par
 /bin/cp SurfExchange_unit1.par SurfExchange_unit1_p3.par
 
-mpirun -np 1 $NMESH SurfExchange_unit1_p1.par > SurfExchange_unit1.out1
-mpirun -np 2 $NMESH SurfExchange_unit1_p2.par > SurfExchange_unit1.out2
-mpirun -np 3 $NMESH SurfExchange_unit1_p2.par > SurfExchange_unit1.out3
+mpirun -np 1 $NMESH SurfExchange_unit1_p1.par > SurfExchange_unit1_p1.out
+mpirun -np 2 $NMESH SurfExchange_unit1_p2.par > SurfExchange_unit1_p2.out
+mpirun -np 3 $NMESH SurfExchange_unit1_p3.par > SurfExchange_unit1_p3.out
 
 $FDIFF -at 1e-13 -rt 1e-12 SurfExchange_unit1_p1_corr SurfExchange_unit1_p1
 $FDIFF -at 1e-13 -rt 1e-12 SurfExchange_unit1_p2_corr SurfExchange_unit1_p2
