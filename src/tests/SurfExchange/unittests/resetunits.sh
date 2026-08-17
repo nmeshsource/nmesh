0b6c2bf6af10e54b84a8a074487fa5c1a3bfb24e
wolf <wolf@tyrfing>
2026-08-17 13:46:00 -0400

    fix

--- src/tests/SurfExchange/unittests/resetunits.sh
+++ src/tests/SurfExchange/unittests/resetunits.sh
@@ -8,6 +8,8 @@ NMESH=$TOP/exe/nmesh
 /bin/cp SurfExchange_unit1.par SurfExchange_unit1_p2.par
 /bin/cp SurfExchange_unit1.par SurfExchange_unit1_p3.par
 
+rm -rf SurfExchange_unit1_p?_corr
+
 mpirun -np 1 $NMESH SurfExchange_unit1_p1.par > SurfExchange_unit1_p1.out
 mv -v SurfExchange_unit1_p1 SurfExchange_unit1_p1_corr
 
